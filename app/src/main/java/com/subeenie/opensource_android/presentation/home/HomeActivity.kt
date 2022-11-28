2cc102bd7d738ee213ca0de0845e0f3993a960e9
kimsubeen <papajj06@naver.com>
2022-11-28 18:14:15 +0900

    [feature/#1] packaging

---
+++ app/src/main/java/com/subeenie/opensource_android/presentation/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.subeenie.opensource_android.presentation.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.subeenie.opensource_android.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
