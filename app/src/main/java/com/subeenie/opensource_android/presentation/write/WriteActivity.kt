ac2e435a5c2c4669de527557457e8a724efa16da
kimsubeen <papajj06@naver.com>
2022-11-29 19:01:50 +0900

    [feature/#3] add writeActivity

---
+++ app/src/main/java/com/subeenie/opensource_android/presentation/write/WriteActivity.kt
@@ -0,0 +1,12 @@
+package com.subeenie.opensource_android.presentation.write
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.subeenie.opensource_android.R
+
+class WriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_write)
+    }
+}
