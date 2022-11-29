8e5d368a5016ed7d691117605fbb006d5baa6f32
kimsubeen <papajj06@naver.com>
2022-11-29 19:03:25 +0900

    [feature/#3] 상세보기View file 추가

---
+++ app/src/main/java/com/subeenie/opensource_android/presentation/detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.subeenie.opensource_android.presentation.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.subeenie.opensource_android.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
