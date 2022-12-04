ec161210bc86f96fa08de55e47860fa1c55c656b
kimsubeen <papajj06@naver.com>
2022-12-04 16:47:39 +0900

    [feature/#5] 글쓰기 뷰 및 로직 구현

--- app/src/main/java/com/subeenie/opensource_android/presentation/write/WriteActivity.kt
+++ app/src/main/java/com/subeenie/opensource_android/presentation/write/WriteActivity.kt
@@ -1,12 +1,31 @@
 package com.subeenie.opensource_android.presentation.write
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.subeenie.opensource_android.R
+import com.subeenie.opensource_android.databinding.ActivityHomeBinding
+import com.subeenie.opensource_android.databinding.ActivityWriteBinding
+import com.subeenie.opensource_android.presentation.home.HomeActivity
 
 class WriteActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWriteBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_write)
+        binding = ActivityWriteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        gotoBack()
     }
+
+    private fun gotoBack() {
+        binding.btnBack.setOnClickListener {
+            finish()
+        }
+        binding.btnComplete.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
 }
