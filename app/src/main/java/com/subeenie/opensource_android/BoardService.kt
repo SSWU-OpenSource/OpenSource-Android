eb7f25f6553c49b99271e5c3f353b8413c84fe93
kimsubeen <papajj06@naver.com>
2022-12-15 14:40:11 +0900

    [feature/#10] Service 생성

--- app/src/main/java/com/subeenie/opensource_android/BoardService.kt
+++ app/src/main/java/com/subeenie/opensource_android/BoardService.kt
@@ -1,31 +1,47 @@
 package com.subeenie.opensource_android
 
+import com.subeenie.opensource_android.data.remote.request.RequestBoard
+import com.subeenie.opensource_android.data.remote.response.ResponseBoard
+import com.subeenie.opensource_android.data.remote.response.ResponseScrap
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface BoardService {
 
     // 전체 게시글 조회
-   //@GET("api/post")
-   // fun getPostInfo(): Call<List<ResponseBoard>>
+    //@GET("api/post")
+    // fun getPostInfo(): Call<List<ResponseBoard>>
 
-   // 상세보기
-   /* @GET("api/post/{postId}")
-    fun getReadInfo(
-        @Path("postId") postId: Int
-    ): Call<ResponseDetail>*/
+    //게시글 작성
+    //@POST("api/post")
+    // fun postBoard(
+    //     @Body body: RequestBoard
+    //): Call<ResponseBoard>
+
+    // 상세보기
+    /* @GET("api/post/{postId}")
+     fun getReadInfo(
+         @Path("postId") postId: Int
+     ): Call<ResponseDetail>*/
 
     //스크랩 게시글
     //@GET("api/scrap")
     // fun getScrapBoardInfo(): Call<List<ResponseBoard>>
+
+    //스크랩 하기
+    //@POST("api/scrap")
+    //fun postScrap(
+    //    @Body body: ResponseScrap
+    //): Call <ResponseScrap>
 
     //스크랩 여부
     //@GET("api/scrap/{postId}")
     // fun getScrapInfo(
     //   @Path("postId") postId:Int
     //   ): Call<List<ResponseScrap>>
-
 
 
 }
