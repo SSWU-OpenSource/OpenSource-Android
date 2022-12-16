db95f4aada4bd038ab252274af3056d144651df5
kimsubeen <papajj06@naver.com>
2022-12-16 11:10:28 +0900

    [feature/#10] servicecreator 수젖ㅇ

--- app/src/main/java/com/subeenie/opensource_android/ServiceCreator.kt
+++ app/src/main/java/com/subeenie/opensource_android/ServiceCreator.kt
@@ -3,37 +3,28 @@ package com.subeenie.opensource_android
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Response
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.io.IOException
 
 object ServiceCreator {
-    private const val BASE_URL =""
 
-    private val retrofit: Retrofit = Retrofit.Builder()
-        .baseUrl(BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-
-
-    private fun provideOkHttpClient(
-        interceptor: AppInterceptor
-    ): OkHttpClient = OkHttpClient.Builder()
-        .run {
-            addInterceptor(interceptor)
-            build()
-        }
-
-    class AppInterceptor : Interceptor {
-        @Throws(IOException::class)
-        override fun intercept(chain: Interceptor.Chain)
-                : Response = with(chain) {
-            val newRequest = request().newBuilder()
-                .addHeader("accept", "application/vnd.github.v3+json")
-                .build()
-
-            proceed(newRequest)
-        }
+    private val client by lazy {
+        OkHttpClient.Builder()
+            .addInterceptor(
+                HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                }
+            ).build()
     }
+    val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl("http://opensource.sample.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+    }
+    inline fun <reified T> create(): T = retrofit.create<T>(T::class.java)
     val boardService: BoardService = retrofit.create(BoardService::class.java)
 }
